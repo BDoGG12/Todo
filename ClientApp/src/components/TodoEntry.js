465dfb3a27c7715cfe39a59467705f4b1d3496ec
Ben Do <bdo.appworkshop@gmail.com>
2021-10-30 11:28:04 -0500

    Refactored the state object and render function. Inserted functions for onChange handlers.

--- ClientApp/src/components/TodoEntry.js
+++ ClientApp/src/components/TodoEntry.js
@@ -1,4 +1,6 @@
 ﻿import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
 
 class TodoEntry extends Component {
     constructor(props) {
@@ -7,21 +9,57 @@ class TodoEntry extends Component {
         this.state = {
             nameEntry: '',
             descEntry: '',
-            isDone: false
+            isDone: false,
+            data = []
         };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleDescChange = this.handleDescChange.bind(this);
+        this.handleIsDoneChange = this.handleIsDoneChange.bind(this);
     }
 
+    handleNameChange(e) {
+        this.setState({ nameEntry: e.target.value });
+    }
 
+    handleDescChange(e) {
+        this.setState({ descEntry: e.target.value });
+    }
+
+    handleIsDoneChange(e) {
+        this.setState({ isDone: !this.state.isDone });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.state.data.push({
+            name: this.state.nameEntry,
+            desc: this.state.descEntry
+        });
+    }
 
     render() {
-        <form>
-            <label>
-                Name:
+        return (
+            <div>
+                <p>
+                    Here are your Todo items for the day:
+                    </p>
+                <form onSubmit={this.handleSubmit} >
+                    <label>
+                        Name:
                 <input type="text" value={this.state.nameEntry} />
                 Description:
                 <input type="text" value={this.state.descEntry} />
-            </label>
-            <input type="submit" value="Add" />
-        </form>
-                }
+                    </label>
+                    <input type="submit" value="Add" />
+                </form>
+                <table>
+                    <TodoItem />
+                </table>
+            </div>
+        )
+
+    }
 }
+
+export default TodoEntry;
